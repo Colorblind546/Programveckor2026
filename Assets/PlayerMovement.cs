3e9f49afd8c11a6e96162ca7d9ddf556879b54bf
Colorblind546 <morgan.marinbeishline@elev.ga.lbs.se>
2026-01-08 12:28:15 +0100

    Movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    float moveX;
+    float moveZ;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        moveX = Input.GetAxis("Horizontal");
+        moveZ = Input.GetAxis("Vertical");
+
+        Debug.Log($"X:{moveX}  Y:{moveZ}");
+
+        transform.position += new Vector3(moveX * 10, 0, moveZ * 10) * Time.deltaTime;
+
+
     }
 }
